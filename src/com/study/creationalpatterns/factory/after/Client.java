896525490bae729741796cdf0bec932e009c77f5
jhdl0157 <jhdl0157@naver.com>
2022-04-07 17:33:02 +0900

    White Ship 만들기 성공

--- src/com/study/creationalpatterns/factory/after/Client.java
+++ src/com/study/creationalpatterns/factory/after/Client.java
@@ -3,11 +3,9 @@ package com.study.creationalpatterns.factory.after;
 public class Client {
 
     public static void main(String[] args) {
-        Ship whiteship = WhiteShipFactory.orderShip("Whiteship", "keesun@mail.com");
+        Ship whiteship = new WhiteShipFactory().orderShip("WhiteShip","jhdl0157@naver.com");
         System.out.println(whiteship);
 
-        Ship blackship = WhiteShipFactory.orderShip("Blackship", "keesun@mail.com");
-        System.out.println(blackship);
     }
 
 }
---
+++ src/com/study/creationalpatterns/factory/after/WhiteShip.java
@@ -0,0 +1,9 @@
+package com.study.creationalpatterns.factory.after;
+
+public class WhiteShip extends Ship{
+    public WhiteShip(){
+        setName("WhiteShip");
+        setLogo("\uD83D\uDEE5️");
+        setColor("White");
+    }
+}
